1a45bc9ddc43358909e70b3b27fca06e439e8125
neck091 <cjstk_05@naver.com>
2024-03-07 13:40:18 +0900

    추가

--- chae_text/src/app/escape/page.js
+++ chae_text/src/app/escape/page.js
@@ -170,11 +170,15 @@ export default () => {
     // 기타 모든 씬에 대한 처리
     setCurrentScene(nextScene);
   };
+  const title =
+    currentScene === "start" || currentScene === "dumy"
+      ? "넓은 들판"
+      : "지하 독방";
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <header className={styles.title}>
-          <h1>지하 독방</h1>
+          <h1>{title}</h1>
         </header>
         <div className={styles.description}>
           {scenarios[currentScene].description}
